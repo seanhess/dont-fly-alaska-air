8f4d10e8fcd7bcf0ee7717dc968b9e077f56d272
Sean Clark Hess <seanhess@gmail.com>
2016-02-02 13:23:20 -0800

    connect working great

--- Main.hs
+++ Main.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.Aeson (FromJSON, fromJSON, Result(..), json, Value)
+import Data.ByteString (ByteString)
 import Data.Monoid ((<>))
-import Control.Monad.Trans.Resource (runResourceT, ResourceT, MonadResource)
-import Control.Concurrent (forkIO)
+import qualified Data.Conduit.Attoparsec as CA
+import Control.Monad.Trans.Resource (runResourceT, ResourceT, MonadResource, MonadThrow, monadThrow)
+import Control.Concurrent (forkIO, threadDelay)
+import Network.HTTP.Types (statusCode)
 import Web.Twitter.Conduit
 import Web.Twitter.Conduit.Stream (stream, statusesFilterByTrack)
+import Web.Twitter.Conduit.Base (makeRequest, getResponse, sinkFromJSON)
 import Web.Twitter.Types.Lens
 import Web.Authenticate.OAuth
-import Network.HTTP.Conduit
+import Network.HTTP.Conduit hiding (responseBody, responseStatus, responseHeaders)
 import Data.Conduit
 import qualified Data.Conduit.List as CL
 import qualified Data.Text as Text
@@ -30,23 +35,18 @@ accountID = 4849803320
 
 main :: IO ()
 main = do
-    (oauth, cred) <- getOAuthTokens
-    let twInfo = setCredential oauth cred def
+    twToken <- getOAuthTokens
+    let twInfo = TWInfo twToken Nothing -- (Just (Proxy "127.0.0.1" 8888))
 
     mgr <- newManager tlsManagerSettings
     timeline <- runResourceT $ do
       -- call twInfo mgr homeTimeline
       liftIO $ putStrLn "STARTING"
 
-      -- ss1 <- stream twInfo mgr (statusesFilterByTrack accountName)
-      ss2 <- stream twInfo mgr (statusesFilterByFollow [4849803320])
+      s <- retryWithDelay 60 (*2) $ streamShowStatus twInfo mgr (statusesFilter accountName [accountID])
 
-      liftIO $ putStrLn "RUNNING"
-
-      -- mapM connectToStream [ss1, ss2]
-      -- mapM conn
-      -- connectToStream ss2
-      ss2 $$+- CL.mapM_ $ \status -> liftIO (handleStream status)
+      liftIO $ putStrLn "CONNECTED"
+      handleStream s
 
       liftIO $ putStrLn "DONE"
 
@@ -56,19 +56,34 @@ main = do
 -- connectStream :: (MonadResource m) => ResumableSource m responseType -> ResourceT IO ()
 -- connectStream ss = ss $$+- CL.mapM_ $ \status -> liftIO (handleStream status)
 
-connectToStream :: ResumableSource (ResourceT IO) StreamingAPI -> ResourceT IO ()
-connectToStream s = do
-    liftIO $ forkIO $ runResourceT $ do
-      s $$+- CL.mapM_ $ \status -> liftIO (handleStream status)
-    return ()
+retryWithDelay :: MonadIO m => Int -> (Int -> Int) -> m (Maybe a) -> m a
+retryWithDelay delay grow action = do
+    ma <- action
+    case ma of
+      Just a -> return a
+      Nothing -> do
+        liftIO $ putStrLn ("FAILED, delaying for " <> show delay <> "s")
+        liftIO $ threadDelay (delay * 1000 * 1000)
+        retryWithDelay (grow delay) grow action
 
-handleStream :: StreamingAPI -> IO ()
-handleStream (SStatus status) = do
+statusesFilterEndpoint :: String
+statusesFilterEndpoint = "https://stream.twitter.com/1.1/statuses/filter.json"
+
+statusesFilter :: Text -> [UserId] -> APIRequest StatusesFilter StreamingAPI
+statusesFilter keyword userIds =
+    APIRequestPost statusesFilterEndpoint [("track", PVString keyword),("follow", PVIntegerArray userIds)]
+
+handleStream :: ResumableSource (ResourceT IO) StreamingAPI -> ResourceT IO ()
+handleStream s = do
+    s $$+- CL.mapM_ $ \status -> liftIO (handleStatus status)
+
+handleStatus :: StreamingAPI -> IO ()
+handleStatus (SStatus status) = do
     putStrLn "STATUS"
     print (status ^. statusText)
     print (shouldRespond status)
     -- print status
-handleStream _ = return ()
+handleStatus _ = return ()
 -- handleStream (SRetweetedStatus _) = putStrLn "RETWEET"
 -- handleStream (SEvent e) = print e
 -- handleStream (SDelete _) = putStrLn "DELETE"
@@ -85,9 +100,8 @@ shouldRespond status =
     (status ^. statusInReplyToStatusId == Nothing)
     -- (status ^. statusInReplyToScreenName == Nothing)
 
-    -- not . Text.isPrefixOf ("@" <> accountName) . Text.toLower . (^. statusText)
 
-getOAuthTokens :: IO (OAuth, Credential)
+getOAuthTokens :: IO TWToken
 getOAuthTokens = do
     consumerKey <- getEnv' "TWITTER_CONSUMER_KEY"
     consumerSecret <- getEnv' "TWITTER_CONSUMER_SECRET"
@@ -101,6 +115,22 @@ getOAuthTokens = do
             [ ("oauth_token", accessToken)
             , ("oauth_token_secret", accessSecret)
             ]
-    return (oauth, cred)
+    return $ TWToken oauth cred
   where
     getEnv' = (S8.pack <$>) . getEnv
+
+
+--------------------------------------
+
+streamShowStatus :: (MonadResource m, FromJSON responseType)
+       => TWInfo
+       -> Manager
+       -> APIRequest apiName responseType
+       -> m (Maybe (ResumableSource m responseType))
+streamShowStatus info mgr req = do
+    rsrc <- getResponse info mgr =<< liftIO (makeRequest req)
+    liftIO $ print (responseStatus rsrc)
+    liftIO $ print (responseHeaders rsrc)
+    case statusCode (responseStatus rsrc) of
+      200 -> return $ Just $ responseBody rsrc $=+ CL.sequence sinkFromJSON
+      _ -> return Nothing
